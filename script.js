497045fa5a5d4945af59f9f1a0e2fbc67a52b8ff
Koulie123 <129706433+Koulie123@users.noreply.github.com>
2024-05-01 22:21:59 -0400

    Added functions to be able to recreate the grid to any size less than 100

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const container = document.querySelector(".container");
-
+const resetButton = document.querySelector("#amountOfGrids");
+const amountOfGridsInput = document.querySelector("#amountOfGridsInput");
 
 
 
 
 const createGrid = function(gridSize) {
+    if (gridSize >= 100){
+        gridSize = 99;
+    }
     for (i = 0; i < gridSize; i++){
         horizontalDiv = document.createElement("div");
         for (j = 0; j < gridSize; j++){
             verticalDiv = document.createElement("div");
             verticalDiv.setAttribute("class", "vertical-div");
-            verticalDiv.textContent = "Hello";
+            // verticalDiv.textContent = "Hello";
+            verticalDiv.addEventListener("mouseenter", function(e) {
+                e.target.style.backgroundColor = "black";
+            });
             horizontalDiv.appendChild(verticalDiv);
         }
         horizontalDiv.setAttribute("class", "horizontal-div");
@@ -18,4 +25,17 @@ const createGrid = function(gridSize) {
         console.log("a horizontal div has been appended.")
     }
 }
+const resetGrid = function(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+const updateGrid = function(){
+    let number = amountOfGridsInput.value;
+    resetGrid();
+    createGrid(number);
+}
+resetButton.addEventListener("click", updateGrid);
+
 createGrid(16);
+
